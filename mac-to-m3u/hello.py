2595270130a3d259dc058003b329575023cde3b1
tv2027 <ph.tr.us2025@gmail.com>
2026-04-17 09:25:47 -0700

    Update hello.py

--- mac-to-m3u/hello.py
+++ mac-to-m3u/hello.py
@@ -1,11 +1,11 @@
 import csv
 
 print ("Hello, World!")
-
+print ("=== Begin of data.csv ===")
 with open('data.csv', mode='r', encoding='utf-8') as file:
     reader = csv.DictReader(file)
     for row in reader:
         print(row)  # Access by header name
-
+print ("=== End of data.csv ===")
 
 
